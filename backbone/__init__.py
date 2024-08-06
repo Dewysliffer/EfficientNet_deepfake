c7075bee761edc70a365d877fddd9cea8e293d4b
Yuyantao <2979706826@qq.com>
2024-08-06 19:03:20 +0800

    update all

---
+++ backbone/__init__.py
@@ -0,0 +1,9 @@
+from .my_model import cbam_EfficientNet
+from .my_model import EfficientNet
+from .utils import (
+    GlobalParams,
+    BlockArgs,
+    BlockDecoder,
+    efficientnet,
+    get_model_params,
+)
